b70a43dc43be74804fcfcb5802d126f48a676ccb
ozzman02 <osantamaria@gmail.com>
2017-08-07 22:09:54 -0600

    Configuracion, views e index controller
    Configuracion, views e index controller

---
+++ proyectos/2-springwebapp/src/com/novellius/controller/IndexController.java
@@ -0,0 +1,39 @@
+package com.novellius.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+/*
+ * Para poner una lista de atributos en la Session :
+ * @SessionAttributes({"resultado", "mensaje"})
+ */
+@Controller
+@SessionAttributes("resultado")
+public class IndexController {
+
+	@RequestMapping("/")
+	public String showIndex(Model model) {
+		model.addAttribute("resultado", "Resultado desde Session");
+		return "index";
+	}
+	
+	/*
+	 * SessionStatus se usa para limpiar un attributo que se haya puesto en la Session.
+	 * Se debe usar el metodo setComplete()
+	 */
+	@RequestMapping("/about")
+	public String showAbout(SessionStatus sessionStatus) {
+		sessionStatus.setComplete();
+		return "about";
+	}
+	
+	@RequestMapping("/admin")
+	public String showAdmin(Model model) {
+		model.addAttribute("mensaje", "Mensaje desde el model");
+		return "admin";
+	}
+	
+}
